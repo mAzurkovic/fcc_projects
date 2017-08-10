12511ed5bb6322bc6350c093430895b29eb995a2
Mattias Zurkovic <mattias@zurkovic.com>
2017-08-10 03:17:56 -0700

    finish params

---
+++ express/program.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var bodyparser = require('body-parser')
+
+var app = express()
+
+app.set('view engine', 'pug')
+//app.set('views', process.argv[3])
+
+//app.use(require('stylus').middleware(process.argv[3]))
+app.use(bodyparser.urlencoded({extended: false}))
+//app.use(express.static(process.argv[3]))
+
+/*app.get('/', function(req, res) {
+  res.end()
+})
+
+app.get('/home', function(req, res) {
+  res.render('index', {date: new Date().toDateString()})
+})*/
+
+app.post('/form', function(req, res) {
+  console.log(req.body.str.split('').reverse().join(''));
+  res.send(req.body.str.split('').reverse().join(''))
+})
+
+app.put('/message/:id', function(req, res) {
+  var id = req.params.id
+  var str = require('crypto')
+    .createHash('sha1')
+    .update(new Date().toDateString() + id)
+    .digest('hex')
+  res.send(str)
+})
+
+
+app.listen(process.argv[2])
